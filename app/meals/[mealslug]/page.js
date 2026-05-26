8fc2703ff4373e1aae458a05d5ca7ee3057f883e
Slim Abida <slimabida21@gmail.com>
2026-05-26 22:51:34 +0200

    Feat :  loading and rendering Meal Details with dynamic route

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,3 +1,33 @@
-export default function mealsDetails() {
-  return <h1>Meals Details</h1>;
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function mealsDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
