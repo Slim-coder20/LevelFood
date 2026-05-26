d3ce04c2f329c4cf15da9342b422d7aa49f1d1aa
Slim Abida <slimabida21@gmail.com>
2026-05-26 23:02:50 +0200

    Fix : modifie the text in the erros.js

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,10 +1,24 @@
 import { getMeal } from "@/lib/meals";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default function mealsDetails({ params }) {
   const meal = getMeal(params.mealSlug);
 
+
+  // On met une condition pour vérfier si le repas existe ou non on utilise la fonction 
+  // NotFound pour générer une page d'erreur 404
+
+  if(!meal){
+    notFound();
+  }
+
+  /**
+   * La méthode replace(/\n/g, "<br/>") est utilisée pour remplacer les sauts de ligne (\n) 
+   * par des balises <br/> dans la propriété instructions de l'objet meal. 
+   * Cela permet de rendre les instructions plus lisibles en les affichant sur plusieurs lignes.
+   */
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
--- app/meals/error.js
+++ app/meals/error.js
@@ -2,8 +2,8 @@
 export default function Error() {
   return (
     <main className="error">
-      <h1>An error occured!</h1>
-      <p>Failed to fetch meal data. Please try again later</p>
+      <h1>Meal not found!</h1>
+      <p>The meal you are looking for does not exist. Please try again.</p>
     </main>
   );
 }
